9c1281e500bde9ee118619fc2a2f9f7b32c1df70
danielconeo <danielyconeob@gmail.com>
2024-05-24 16:48:16 -0500

    Se agrego el proyecto

---
+++ views/empresa/index.php
@@ -0,0 +1,23 @@
+<?php
+include("../../public/includ/head.php");
+include("../../public/includ/nav.php");
+include("../../public/includ/aside.php");
+?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+          <button type="button" class="btn btn-primary">Registrar</button>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    
+<?php
+include("../../public/includ/foot.php");
+?>
