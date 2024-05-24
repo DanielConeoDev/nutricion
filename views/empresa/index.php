7092b8606ef1339c1168472890870e780cc9ef1f
danielconeo <danielyconeob@gmail.com>
2024-05-24 16:59:18 -0500

    Cambio

--- views/empresa/index.php
+++ views/empresa/index.php
@@ -8,7 +8,7 @@ include("../../public/includ/aside.php");
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-          <button type="button" class="btn btn-primary">Registrar</button>
+          <button type="button" class="btn btn-primary">Registrar la Empresa</button>
           </div><!-- /.col -->
           <div class="col-sm-6">
             
