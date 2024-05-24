32300a8f2cfaa8b20a7c77356540f76f1cf117a9
Dilancogollo25 <dcogollomarriaga@gmail.com>
2024-05-24 17:53:58 -0500

    d

--- views/empresa/index.php
+++ views/empresa/index.php
@@ -5,6 +5,7 @@ include("../../public/includ/aside.php");
 ?>
 <!-- Content Header (Page header) -->
 <!-- Content Header (Page header) -->
+<!-- Content Header (Page header) -->
 <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
