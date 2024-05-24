24f1c9510dd0752a49eb3adb49d34ab4735e38a9
danielconeo <danielyconeob@gmail.com>
2024-05-24 18:32:57 -0500

    Formulario Empresa Agregado

--- public/includ/foot.php
+++ public/includ/foot.php
@@ -1,44 +1,61 @@
 </div>
-        <!-- /.content-wrapper -->
+<!-- /.content-wrapper -->
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-            <div class="p-3">
-                <h5>Title</h5>
-                <p>Sidebar content</p>
-            </div>
-        </aside>
-        <!-- /.control-sidebar -->
-
-        <!-- Main Footer -->
-        <footer class="main-footer">
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-                Proyecto
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2024 <a class="text-danger text-decoration-none" href="https://www.unisinu.edu.co/">Universidad del Sinú Elías Bechara Zainúm, Seccional Cartagena</a>.</strong>
-        </footer>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
     </div>
-    <!-- ./wrapper -->
+</aside>
+<!-- /.control-sidebar -->
 
-    <!-- REQUIRED SCRIPTS -->
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Proyecto
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2024 <a class="text-danger text-decoration-none" href="https://www.unisinu.edu.co/">Universidad del Sinú Elías Bechara Zainúm, Seccional Cartagena</a>.</strong>
+</footer>
+</div>
+<!-- ./wrapper -->
 
-    <!-- jQuery -->
-    <script src="../../public/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../public/dist/js/adminlte.min.js"></script>
+<!-- REQUIRED SCRIPTS -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
-    <script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+<!-- jQuery -->
+<script src="../../public/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+<!-- AdminLTE App -->
+<script src="../../public/dist/js/adminlte.min.js"></script>
 
-    <script>
-        new DataTable('#example');
-    </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+
+<script>
+    new DataTable('#example');
+</script>
+
+<script>
+  // Asegúrate de que el identificador del modal en el script coincida con el definido en el HTML
+  const myModal = document.getElementById('exampleModal');
+
+  // Agrega un listener para el evento 'shown.bs.modal' que se dispara cuando el modal se muestra
+  myModal.addEventListener('shown.bs.modal', function () {
+    // Busca un elemento dentro del modal para enfocar
+    const myInput = myModal.querySelector('input');
+    // Si se encuentra el elemento, enfócalo
+    if (myInput) {
+      myInput.focus();
+    }
+  });
+</script>
+
 </body>
 
 </html>
--- views/empresa/index.php
+++ views/empresa/index.php
@@ -8,7 +8,73 @@ include("../../public/includ/aside.php");
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-plus"></i> Empresa</button>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModal">
+          <i class="fa-solid fa-plus"></i> Empresa
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Registrar Empresa</h1>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="row mb-3">
+                    <label for="nit" class="col-sm-2 col-form-label text-end">NIT</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="nit" name="nit" placeholder="Ingrese el NIT">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <label for="nombre" class="col-sm-2 col-form-label text-end">Nombre</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingrese el nombre">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <label for="estado" class="col-sm-2 col-form-label text-end">Estado</label>
+                    <div class="col-sm-10">
+                      <select class="form-select" id="estado" name="estado">
+                        <option selected disabled>Seleccione un estado</option>
+                        <option value="Activo">Activo</option>
+                        <option value="Inactivo">Inactivo</option>
+                        <option value="En proceso">En proceso</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <label for="telefono" class="col-sm-2 col-form-label text-end">Teléfono</label>
+                    <div class="col-sm-10">
+                      <input type="tel" class="form-control" id="telefono" name="telefono" placeholder="Ingrese el teléfono">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <label for="email" class="col-sm-2 col-form-label text-end">Email</label>
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese el email">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <label for="prefijo" class="col-sm-2 col-form-label text-end">Prefijo</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="prefijo" name="prefijo" placeholder="Ingrese el prefijo">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <div class="col-sm-10 offset-sm-2">
+                      <button type="submit" class="btn btn-primary">Guardar</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div><!-- /.col -->
       <div class="col-sm-6">
 
@@ -45,7 +111,7 @@ include("../../public/includ/aside.php");
                   <td>
                     <div class="btn-group">
                       <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      <i class="fa-solid fa-ellipsis-vertical"></i>
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
                       </button>
                       <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#"><i class="fa-solid fa-pen-to-square"></i> Editar</a></li>
