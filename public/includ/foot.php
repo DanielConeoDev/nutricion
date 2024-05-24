07027c84c00a35a5de6ac033fb782e0991b2be97
danielconeo <danielyconeob@gmail.com>
2024-05-24 17:48:54 -0500

    Cambio

--- public/includ/foot.php
+++ public/includ/foot.php
@@ -31,6 +31,14 @@
     <script src="../../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="../../public/dist/js/adminlte.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+
+    <script>
+        new DataTable('#example');
+    </script>
 </body>
 
 </html>
